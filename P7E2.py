72821cb2adc5494f7107a66c33d56a95d26d1360
Adrian Agudo Bruno <aagudo@cifpfbmoll.eu>
2019-11-05 16:32:10 +0100

    Añado la modificación

--- P7E2.py
+++ P7E2.py
@@ -1,5 +1,4 @@
 """AUTOR:Adrián Agudo Bruno 
-   pruebagit
    ENUNCIADO:Escribe un programa que lea el nombre y los dos apellidos de una persona (en tres cadenas de caracteres diferentes), los pase como parámetros a una función, y ésta debe unirlos y devolver una única cadena. La cadena final la imprimirá el programa por pantalla.
 """
 def fun(a,b,c):
